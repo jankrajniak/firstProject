34e99e5e1e5fc390bdc130196973ced391b0fcd8
jankrajniak <jan.krajniak@gmail.com>
2024-11-14 22:32:27 -0500

    finalized JS for recording music. Added comments. Created a library of info about notes to standardize the event listeners for key-presses.

--- assets/js/play.js
+++ assets/js/play.js
@@ -1,38 +1,56 @@
-//Selectors for HTML elements of interest
+//Selectors for all HTML 
 const keysPressed = document.querySelector('#keys-Pressed')
 
 // Logic used to determine whether the page is operating under "recording" or "not recording" behavior
 
-let recording = false;
+    //Intialize variable which tracks the state
+    let recording = false;
 
-recordButton = document.querySelector('#record-button');
+    //Select the html element (button) which the user clicks to start recording music
+    recordButton = document.querySelector('#record-button');
 
-recordButton.addEventListener('click', function() {
-    if (recording === false) {
-        localStorage.removeItem('tempStorage');
-        recording = true;
-        recordButton.textContent = 'Stop Recording';
-    } else {
-        let recordingName = prompt('Please enter the name for your music recording')
-        saveMusic(recordingName);
-        recording = false;
-        recordButton.textContent = 'Record Music';
-        localStorage.removeItem('tempStorage');
-    }
+    //Add an event listenered to the button
+    recordButton.addEventListener('click', function() {
+        // If the "state" was "not recording" before the click, thus we are starting recording
+        if (recording === false) {
+            //Clear all the previously displayed noteObject, so that the user is clear on which notes
+            //are being recorderd
+            removeHTML();
+            //remove the last unrecorded note from temporary storage so that it isn't included in the recording
+            localStorage.removeItem('tempStorage');
+            //set the recording variable to true to that functions behave in the "recording" state
+            recording = true;
+            //change the text of the recording button to show that it's the button to be pushed to stop the recording
+            recordButton.textContent = 'Stop Recording';
+        // Otherwise the state could only be "recording", thus we are stopping recording
+        } else {
+            //prompt the user for a name for their music
+            let recordingName = prompt('Please enter the name for your music recording')
+            //save the recording into local storage under the inputted name
+            saveMusic(recordingName);
+            //change state to "not recording"
+            recording = false;
+            //change the text of the record button back to 'Record Music"
+            recordButton.textContent = 'Record Music';
+            //clear temporary storage so that the display for the user is cleared
+            localStorage.removeItem('tempStorage');
+            //clear the displayed noteObject(s), so that the user is clear they are starting fresh
+            removeHTML();
+        }
 
-})
+    })
 
 // Generic function to create a note object
-function createNote(sound, description) {
+function createNote(sound, description, location,) {
     let noteObject = {
         note: sound,
         name: description,
+        url: location,
     }
     return noteObject;
 }
 
-// Function to store a note object to an array
-
+// Function to retrieve noteObject(s) pressed stored in temporary storage
 function retrieveFromStorage() {
     const tempStorage = localStorage.getItem('tempStorage');
     let currentMusic;
@@ -46,12 +64,14 @@ function retrieveFromStorage() {
     return currentMusic;
 }
 
-// Function to save an array of note objects to temporary storage (used for building HTML and holding played music when recording)
+// Function to update the temporary storage with a noteObject
 function storeNote(noteObject) {
+    //retrieve noteObjects in local storage
     const tempStorage = retrieveFromStorage();
-
+    //If recording, append the noteObject to temporary storage
     if (recording) {
         tempStorage.push(noteObject);
+    //If not recording, replace the ONLY noteObject in temporary storage
     } else {
         if (tempStorage.length === 0) {
             tempStorage.push(noteObject);
@@ -59,6 +79,7 @@ function storeNote(noteObject) {
             tempStorage[0] = noteObject;
         }    
     }
+    //Save new temporary storage to local storage
     localStorage.setItem('tempStorage', JSON.stringify(tempStorage));
 }
 
@@ -69,71 +90,201 @@ function saveMusic(recordingName) {
 }
 
 
-// Function to create HTML elements to represent the notes held in localStorage under tempStorage
-function createHTML(noteObject) {
-    keyPressed = document.querySelector('#noteObject')
+//Function to clear all HTML elements with class noteObject from the 'keys-pressed' div, 
+//so that the display can be refreshed
+function removeHTML () {
+    const notesDisplayed = document.querySelectorAll('.noteObject')
+    notesDisplayed.forEach(element => {
+        element.remove();
+})
+}
 
-    if (keyPressed) {
-        keyPressed.querySelector('h2').textContent = noteObject.note;
-        keyPressed.querySelector('p').textContent = noteObject.name;
-    } else {
-        const note = document.createElement('div');
-        note.setAttribute('id','noteObject');
-    
-        const key = document.createElement('h2');
-        key.textContent = noteObject.note;
+// Function to create HTML elements to represent the notes held in localStorage under tempStorage
+function createHTML() {
+    //Clear HTML of noteOBject(s)
+    removeHTML();
+
+    const tempStorage = retrieveFromStorage();
+
+        tempStorage.forEach(noteObject => {
+            const note = document.createElement('div');
+            note.setAttribute('class','noteObject');
         
-        const name = document.createElement('p');
-        name.textContent = noteObject.name;
-    
-        note.appendChild(key); 
-        note.appendChild(name);
-    
-        keysPressed.appendChild(note);
-    }
+            const key = document.createElement('h2');
+            key.textContent = noteObject.note;
+            
+            const name = document.createElement('p');
+            name.textContent = noteObject.name;
+        
+            note.appendChild(key); 
+            note.appendChild(name);
+        
+            keysPressed.appendChild(note);
+        })
     
 }
 
-// Logic to create a specific note object on key press or mouse click (event listeners for each keypress)
+// Event listeners for each key press. These listeners repeat for each notes, comments will only be included
+// on the first one
 Bb.addEventListener('click', function() {  
-    const note = new Audio('./assets/sounds/Bb.wav');
+
+    filename = './assets/sounds/Bb.wav'
+    //create an audio object with the sound appropriate for the key
+    const note = new Audio(filename);
+    //play the audio object
     note.play();
 
-    playedNote = createNote(this.id,'B flat');
+    //create a noteObject for the paino key pressed
+    playedNote = createNote(this.id,'Note B in flat',filename);
 
+    //store the noteObjected in temporary storage (appropriate to recording or not recording state)
     storeNote(playedNote);
 
-    createHTML(playedNote);
+    //create the HTML elements to display
+    createHTML();
 
 });
 
 Fsharp.addEventListener('click', function() {   
-    const note = new Audio('./assets/sounds/Fsharp.wav');
+    filename = './assets/sounds/Fsharp.wav'
+    const note = new Audio(filename);
     note.play();
 
-    playedNote = createNote(this.id,'F sharp');
+    playedNote = createNote(this.id,'Note F in sharp',filename);
 
     storeNote(playedNote);
 
-    createHTML(playedNote);
+    createHTML();
 
 });
 
-Eb.addEventListener('click', function() {
-
-    const note = new Audio('./assets/sounds/Eb.wav');
+Eb.addEventListener('click', function() {   
+    filename = './assets/sounds/Eb.wav'
+    const note = new Audio(filename);
     note.play();
 
-    playedNote = createNote(this.id,'E flat');
+    playedNote = createNote(this.id,'Note E in flat',filename);
 
     storeNote(playedNote);
 
-    createHTML(playedNote);
+    createHTML();
 
 });
 
+Csharp.addEventListener('click', function() {   
+    filename = './assets/sounds/Csharp.wav'
+    const note = new Audio(filename);
+    note.play();
 
+    playedNote = createNote(this.id,'Note C in sharp',filename);
 
+    storeNote(playedNote);
 
+    createHTML();
 
+});
+
+Gsharp.addEventListener('click', function() {   
+    filename = './assets/sounds/Gsharp.wav'
+    const note = new Audio(filename);
+    note.play();
+
+    playedNote = createNote(this.id,'Note G in sharp',filename);
+
+    storeNote(playedNote);
+
+    createHTML();
+
+});
+
+B.addEventListener('click', function() {   
+    filename = './assets/sounds/B.wav'
+    const note = new Audio(filename);
+    note.play();
+
+    playedNote = createNote(this.id,'Note B',filename);
+
+    storeNote(playedNote);
+
+    createHTML();
+
+});
+
+F.addEventListener('click', function() {   
+    filename = './assets/sounds/F.wav'
+    const note = new Audio(filename);
+    note.play();
+
+    playedNote = createNote(this.id,'Note F',filename);
+
+    storeNote(playedNote);
+
+    createHTML();
+
+});
+
+A.addEventListener('click', function() {   
+    filename = './assets/sounds/A.wav'
+    const note = new Audio(filename);
+    note.play();
+
+    playedNote = createNote(this.id,'Note A',filename);
+
+    storeNote(playedNote);
+
+    createHTML();
+
+});
+
+E.addEventListener('click', function() {   
+    filename = './assets/sounds/E.wav'
+    const note = new Audio(filename);
+    note.play();
+
+    playedNote = createNote(this.id,'Note E',filename);
+
+    storeNote(playedNote);
+
+    createHTML();
+
+});
+
+D.addEventListener('click', function() {   
+    filename = './assets/sounds/D.wav'
+    const note = new Audio(filename);
+    note.play();
+
+    playedNote = createNote(this.id,'Note D',filename);
+
+    storeNote(playedNote);
+
+    createHTML();
+
+});
+
+C.addEventListener('click', function() {   
+    filename = './assets/sounds/C.wav'
+    const note = new Audio(filename);
+    note.play();
+
+    playedNote = createNote(this.id,'Note C',filename);
+
+    storeNote(playedNote);
+
+    createHTML();
+
+});
+
+G.addEventListener('click', function() {   
+    filename = './assets/sounds/G.wav'
+    const note = new Audio(filename);
+    note.play();
+
+    playedNote = createNote(this.id,'Note G',filename);
+
+    storeNote(playedNote);
+
+    createHTML();
+
+});
 
