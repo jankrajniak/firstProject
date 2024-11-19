e033a3fba74245389e05b8b08ab8ac88cf571e7e
jankrajniak <jan.krajniak@gmail.com>
2024-11-18 20:09:54 -0500

    corrected note associations to sounds

--- assets/js/sounds.js
+++ assets/js/sounds.js
@@ -19,8 +19,8 @@ const G = document.querySelector('#G');
 
 
 const allNotes = [{
-    id:'#D-sharp',
-    text: 'Note D in sharp',
+    id:'#A-sharp',
+    text: 'Note A in sharp',
     url: './assets/sounds/Bb.wav'
 },
 
@@ -31,8 +31,8 @@ const allNotes = [{
 },
 
 {
-    id:'#A-sharp',
-    text: 'Note A in sharp',
+    id:'#D-sharp',
+    text: 'Note D in sharp',
     url: './assets/sounds/Eb.wav'
 },
 
